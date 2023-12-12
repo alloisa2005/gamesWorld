d4183177e29d417af8ee883409d50cde3eaea176
Anthony Allois <anthonyallois@gmail.com>
2023-12-12 16:20:54 -0300

    Ajustes en Funciones de Imagenes

--- src/utils/uploadImages.js
+++ src/utils/uploadImages.js
@@ -3,7 +3,7 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export const uploadImageToStorage = async (file, folder='') => {
 
-  try {
+  try {    
     const storageRef = ref(storage, `${folder}/${file.name}`);
     const fileSnapshot = await uploadBytes(storageRef, file);
     const imgUrl = await getDownloadURL(fileSnapshot.ref);
@@ -16,49 +16,4 @@ export const uploadImageToStorage = async (file, folder='') => {
 
 };
 
-// export const uploadGameImage = async (file) => {
-
-//   try {
-//     if(!file) return {error:false, errorMsg: '', imgUrl: ''}; // No file selected (or file is null
-    
-//     const image = new FormData();
-//     image.append("file", file);
-//     image.append("cloud_name", 'dayyupv1u');
-//     image.append("upload_preset", 'gns-games');                  
-
-//     const res = await fetch('http://api.cloudinary.com/v1_1/dayyupv1u/image/upload', {
-//         method: "POST",
-//         body: image,
-//     });
-
-//     const data = await res.json();         
-    
-//     return {error:false, errorMsg: '', imgUrl: data.secure_url};
-
-//   } catch (error) {      
-//     return {error:true, errorMsg: error.message, imgUrl: ''};      
-//   }    
-// }
-
-// export const uploadUserImage = async (file) => {
-
-//   try {
-//     const image = new FormData();
-//     image.append("file", file);
-//     image.append("cloud_name", 'dayyupv1u');
-//     image.append("upload_preset", 'gns-users');              
-
-//     const res = await fetch('http://api.cloudinary.com/v1_1/dayyupv1u/image/upload', {
-//         method: "POST",
-//         body: image,
-//     });
-
-//     const data = await res.json();             
-
-//     return {error:false, errorMsg: '', imgUrl: data.secure_url};
-
-//   } catch (error) {      
-//     return {error:true, errorMsg: error.message, imgUrl: ''}; 
-//   }  
-// }
 
