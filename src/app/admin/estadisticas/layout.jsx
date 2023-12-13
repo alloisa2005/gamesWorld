ab259a6f72a732302de20a2d3a43c91cfda5d365
Anthony Allois <anthonyallois@gmail.com>
2023-12-13 17:33:41 -0300

    Ajustes en stock

--- src/app/admin/estadisticas/layout.jsx
+++ src/app/admin/estadisticas/layout.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EstadisticasLayout = ({ userMasCompras, comprasMes, juegosmasvendidos }) => {  
 
   return (
-    <div className='grid grid-cols-8 grid-rows-2 gap-3 h-[500px]'>  
+    <div className='grid grid-cols-8 grid-rows-2 gap-3 h-[500px] mb-5'>  
       { comprasMes }  
       { userMasCompras }
       { juegosmasvendidos }
--- src/db/firebase.js
+++ src/db/firebase.js
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyiN115dBaoE_3mLCDAKUOoAJzbW1l52A",
-  authDomain: "games-world-c68fd.firebaseapp.com",
-  projectId: "games-world-c68fd",
-  storageBucket: "games-world-c68fd.appspot.com",
-  messagingSenderId: "767618946604",
-  appId: "1:767618946604:web:70edde5158cbe5e596e389"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
